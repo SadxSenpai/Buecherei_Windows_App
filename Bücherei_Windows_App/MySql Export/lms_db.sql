19991316ef565111a3528151d011ffa6769efdaf
SadxSenpai <Marvinhauser14@gmail.com>
2023-02-15 12:49:20 +0100

    Final

--- Bücherei_Windows_App/MySql Export/lms_db.sql
+++ Bücherei_Windows_App/MySql Export/lms_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2023 at 04:37 PM
+-- Generation Time: Feb 15, 2023 at 12:48 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -32,7 +32,7 @@ CREATE TABLE `app_users` (
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(256) NOT NULL,
   `user_type` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -41,8 +41,8 @@ CREATE TABLE `app_users` (
 --
 
 INSERT INTO `app_users` (`id`, `first_name`, `last_name`, `email`, `password`, `user_type`) VALUES
-(1, 'Marvin', 'Hauser', 'Admin@LMS.de', 'Admin', 'admin'),
-(5, '1', '1', 'qwer@qwer.de', '1', 'admin');
+(1, 'Marvin', 'Hauser', 'Admin@LMS.de', 'c1c224b03cd9bc7b6a86d77f5dace40191766c485cd55dc48caf9ac873335d6f', 'admin'),
+(10, 'Test', 'Test', 'test@test.de', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'admin');
 
 -- --------------------------------------------------------
 
@@ -67,10 +67,25 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`id`, `book_name`, `book_author`, `book_type`, `book_out_with`, `book_out_since`, `book_back_when`, `book_out`, `book_note`) VALUES
-(1, 'Buch1', 'Bob', 'booktype', 'bookuser', 'dateout', 'dateback', 1, 'bookinfo'),
-(2, 'Eindingsbums', 'Bobby', 'booktype', 'bookuser', 'dateout', 'dateback', 1, 'bookinfo'),
-(3, 'PC Gaymes', 'Krampus', 'booktype', 'bookuser', 'dateout', 'dateback', 1, 'bookinfo'),
-(4, 'not PC Gaymes', 'Krampus 2.0', 'booktype', 'bookuser', 'dateout', 'dateback', 1, 'bookinfo');
+(2, 'Eindingsbums', 'Bobby', 'Heft', 'Rei', '10/02/2023', '17/02/2023', 1, 'Riss auf seite 2, Kaffeefleck'),
+(3, 'PC Gaymes', 'Krampus', '', '', '', '', 0, 'Knick auf dem Cover, lsjkdfjksa'),
+(4, 'not PC Gaymes', 'Krampus 2.0', 'Buch', 'Rei', '10/02/2023', '17/02/2023', 1, '-----'),
+(6, 'Test', 'Test', 'Buch', 'Marco', '15/02/2023', '22/02/2023', 1, 'Test, test 2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deleted_books`
+--
+
+CREATE TABLE `deleted_books` (
+  `id` int(100) NOT NULL,
+  `book_name` varchar(100) NOT NULL,
+  `book_author` varchar(100) NOT NULL,
+  `book_type` varchar(100) NOT NULL,
+  `del_when` varchar(100) NOT NULL,
+  `del_why` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -89,6 +104,12 @@ ALTER TABLE `books`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `deleted_books`
+--
+ALTER TABLE `deleted_books`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -96,13 +117,19 @@ ALTER TABLE `books`
 -- AUTO_INCREMENT for table `app_users`
 --
 ALTER TABLE `app_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `deleted_books`
+--
+ALTER TABLE `deleted_books`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

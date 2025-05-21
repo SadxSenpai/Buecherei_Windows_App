7e5482c0e84b78212e7e3c075ae3b15a22adf028
SadxSenpai <Marvinhauser14@gmail.com>
2025-05-21 08:27:18 +0200

    Unfucked the fucked forms not loading propperly

--- Bücherei_Windows_App/UserControl/Item_Out_UC.Designer.cs
+++ Bücherei_Windows_App/UserControl/Item_Out_UC.Designer.cs
@@ -45,6 +45,7 @@
             exit_label = new Label();
             label4 = new Label();
             panel1.SuspendLayout();
+            SuspendLayout();
             // 
             // panel1
             // 
@@ -74,7 +75,7 @@
             item_date_in_cb.DropDownStyle = ComboBoxStyle.DropDownList;
             item_date_in_cb.FocusedColor = Color.FromArgb(94, 148, 255);
             item_date_in_cb.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
-            item_date_in_cb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            item_date_in_cb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             item_date_in_cb.ForeColor = Color.FromArgb(68, 88, 112);
             item_date_in_cb.ItemHeight = 30;
             item_date_in_cb.Location = new Point(176, 343);
@@ -89,7 +90,7 @@
             item_name_cb.DropDownStyle = ComboBoxStyle.DropDownList;
             item_name_cb.FocusedColor = Color.FromArgb(94, 148, 255);
             item_name_cb.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
-            item_name_cb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            item_name_cb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             item_name_cb.ForeColor = Color.FromArgb(68, 88, 112);
             item_name_cb.ItemHeight = 30;
             item_name_cb.Location = new Point(176, 51);
@@ -108,32 +109,32 @@
             // 
             // item_date_out_tb
             // 
-            item_date_out_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            item_date_out_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             item_date_out_tb.Location = new Point(176, 269);
             item_date_out_tb.Name = "item_date_out_tb";
             item_date_out_tb.ReadOnly = true;
-            item_date_out_tb.Size = new Size(207, 33);
+            item_date_out_tb.Size = new Size(207, 39);
             item_date_out_tb.TabIndex = 36;
             // 
             // item_with_who_tb
             // 
-            item_with_who_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            item_with_who_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             item_with_who_tb.Location = new Point(176, 195);
             item_with_who_tb.Name = "item_with_who_tb";
-            item_with_who_tb.Size = new Size(207, 33);
+            item_with_who_tb.Size = new Size(207, 39);
             item_with_who_tb.TabIndex = 35;
             // 
             // item_type_tb
             // 
-            item_type_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            item_type_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             item_type_tb.Location = new Point(176, 121);
             item_type_tb.Name = "item_type_tb";
-            item_type_tb.Size = new Size(207, 33);
+            item_type_tb.Size = new Size(207, 39);
             item_type_tb.TabIndex = 34;
             // 
             // label7
             // 
-            label7.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             label7.ForeColor = Color.White;
             label7.Location = new Point(3, 416);
             label7.Name = "label7";
@@ -144,7 +145,7 @@
             // 
             // label6
             // 
-            label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             label6.ForeColor = Color.White;
             label6.Location = new Point(3, 343);
             label6.Name = "label6";
@@ -155,7 +156,7 @@
             // 
             // label5
             // 
-            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             label5.ForeColor = Color.White;
             label5.Location = new Point(3, 270);
             label5.Name = "label5";
@@ -176,7 +177,7 @@
             // 
             // label3
             // 
-            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             label3.ForeColor = Color.White;
             label3.Location = new Point(3, 197);
             label3.Name = "label3";
@@ -187,7 +188,7 @@
             // 
             // label2
             // 
-            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             label2.ForeColor = Color.White;
             label2.Location = new Point(3, 124);
             label2.Name = "label2";
@@ -198,7 +199,7 @@
             // 
             // label1
             // 
-            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             label1.ForeColor = Color.White;
             label1.Location = new Point(3, 51);
             label1.Name = "label1";
@@ -210,10 +211,10 @@
             // exit_label
             // 
             exit_label.AutoSize = true;
-            exit_label.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            exit_label.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             exit_label.Location = new Point(374, 0);
             exit_label.Name = "exit_label";
-            exit_label.Size = new Size(30, 32);
+            exit_label.Size = new Size(38, 41);
             exit_label.TabIndex = 1;
             exit_label.Text = "X";
             exit_label.Click += exit_label_Click;
@@ -223,12 +224,28 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             label4.Location = new Point(0, 0);
             label4.Name = "label4";
-            label4.Size = new Size(176, 32);
+            label4.Size = new Size(217, 41);
             label4.TabIndex = 6;
             label4.Text = "Buch Ausgabe";
+            // 
+            // Book_Out_UC
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Controls.Add(label4);
+            Controls.Add(exit_label);
+            Controls.Add(panel1);
+            Name = "Book_Out_UC";
+            Size = new Size(404, 681);
+            Load += Book_Out_UC_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
--- Bücherei_Windows_App/UserControl/Item_add_UC.Designer.cs
+++ Bücherei_Windows_App/UserControl/Item_add_UC.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            item_name_tb = new TextBox();
             item_note_tb = new TextBox();
             item_type_tb = new TextBox();
             item_id_tb = new TextBox();
@@ -43,8 +44,8 @@
             label1 = new Label();
             exit_label = new Label();
             label4 = new Label();
-            item_name_tb = new TextBox();
             panel1.SuspendLayout();
+            SuspendLayout();
             // 
             // panel1
             // 
@@ -62,100 +63,116 @@
             panel1.Controls.Add(item_add_finish_btn);
             panel1.Controls.Add(label2);
             panel1.Controls.Add(label1);
-            panel1.Location = new Point(3, 35);
+            panel1.Location = new Point(3, 47);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(398, 643);
+            panel1.Size = new Size(455, 857);
             panel1.TabIndex = 0;
+            // 
+            // item_name_tb
+            // 
+            item_name_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
+            item_name_tb.Location = new Point(200, 63);
+            item_name_tb.Margin = new Padding(3, 4, 3, 4);
+            item_name_tb.Name = "item_name_tb";
+            item_name_tb.Size = new Size(236, 39);
+            item_name_tb.TabIndex = 33;
             // 
             // item_note_tb
             // 
-            item_note_tb.Location = new Point(175, 417);
+            item_note_tb.Location = new Point(200, 556);
+            item_note_tb.Margin = new Padding(3, 4, 3, 4);
             item_note_tb.Multiline = true;
             item_note_tb.Name = "item_note_tb";
-            item_note_tb.Size = new Size(208, 96);
+            item_note_tb.Size = new Size(237, 127);
             item_note_tb.TabIndex = 32;
             // 
             // item_type_tb
             // 
-            item_type_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            item_type_tb.Location = new Point(176, 343);
+            item_type_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
+            item_type_tb.Location = new Point(201, 457);
+            item_type_tb.Margin = new Padding(3, 4, 3, 4);
             item_type_tb.Name = "item_type_tb";
-            item_type_tb.Size = new Size(207, 33);
+            item_type_tb.Size = new Size(236, 39);
             item_type_tb.TabIndex = 31;
             // 
             // item_id_tb
             // 
-            item_id_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            item_id_tb.Location = new Point(176, 269);
+            item_id_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
+            item_id_tb.Location = new Point(201, 359);
+            item_id_tb.Margin = new Padding(3, 4, 3, 4);
             item_id_tb.Name = "item_id_tb";
-            item_id_tb.Size = new Size(207, 33);
+            item_id_tb.Size = new Size(236, 39);
             item_id_tb.TabIndex = 30;
             // 
             // item_origin_tb
             // 
-            item_origin_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            item_origin_tb.Location = new Point(176, 195);
+            item_origin_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
+            item_origin_tb.Location = new Point(201, 260);
+            item_origin_tb.Margin = new Padding(3, 4, 3, 4);
             item_origin_tb.Name = "item_origin_tb";
-            item_origin_tb.Size = new Size(207, 33);
+            item_origin_tb.Size = new Size(236, 39);
             item_origin_tb.TabIndex = 29;
             // 
             // item_count_tb
             // 
-            item_count_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            item_count_tb.Location = new Point(176, 121);
+            item_count_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
+            item_count_tb.Location = new Point(201, 161);
+            item_count_tb.Margin = new Padding(3, 4, 3, 4);
             item_count_tb.Name = "item_count_tb";
-            item_count_tb.Size = new Size(207, 33);
+            item_count_tb.Size = new Size(236, 39);
             item_count_tb.TabIndex = 28;
             // 
             // label6
             // 
-            label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             label6.ForeColor = Color.White;
-            label6.Location = new Point(3, 124);
+            label6.Location = new Point(3, 165);
             label6.Name = "label6";
-            label6.Size = new Size(156, 23);
+            label6.Size = new Size(178, 31);
             label6.TabIndex = 26;
             label6.Text = "item_count";
             label6.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
-            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             label3.ForeColor = Color.White;
-            label3.Location = new Point(3, 270);
+            label3.Location = new Point(3, 360);
             label3.Name = "label3";
-            label3.Size = new Size(156, 23);
+            label3.Size = new Size(178, 31);
             label3.TabIndex = 24;
             label3.Text = "item_id";
             label3.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label7
             // 
-            label7.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             label7.ForeColor = Color.White;
-            label7.Location = new Point(3, 416);
+            label7.Location = new Point(3, 555);
             label7.Name = "label7";
-            label7.Size = new Size(156, 23);
+            label7.Size = new Size(178, 31);
             label7.TabIndex = 13;
             label7.Text = "item_note";
             label7.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label5
             // 
-            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             label5.ForeColor = Color.White;
-            label5.Location = new Point(3, 197);
+            label5.Location = new Point(3, 263);
             label5.Name = "label5";
-            label5.Size = new Size(156, 23);
+            label5.Size = new Size(178, 31);
             label5.TabIndex = 9;
             label5.Text = "item_origin";
             label5.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // item_add_finish_btn
             // 
-            item_add_finish_btn.Location = new Point(67, 582);
+            item_add_finish_btn.Location = new Point(77, 776);
+            item_add_finish_btn.Margin = new Padding(3, 4, 3, 4);
             item_add_finish_btn.Name = "item_add_finish_btn";
-            item_add_finish_btn.Size = new Size(250, 42);
+            item_add_finish_btn.Size = new Size(286, 56);
             item_add_finish_btn.TabIndex = 7;
             item_add_finish_btn.Text = "Hinzufügen";
             item_add_finish_btn.UseVisualStyleBackColor = true;
@@ -163,22 +180,22 @@
             // 
             // label2
             // 
-            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             label2.ForeColor = Color.White;
-            label2.Location = new Point(3, 343);
+            label2.Location = new Point(3, 457);
             label2.Name = "label2";
-            label2.Size = new Size(156, 23);
+            label2.Size = new Size(178, 31);
             label2.TabIndex = 2;
             label2.Text = "item_type";
             label2.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label1
             // 
-            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(3, 51);
+            label1.Location = new Point(3, 68);
             label1.Name = "label1";
-            label1.Size = new Size(156, 23);
+            label1.Size = new Size(178, 31);
             label1.TabIndex = 0;
             label1.Text = "Item_name";
             label1.TextAlign = ContentAlignment.MiddleCenter;
@@ -186,10 +203,10 @@
             // exit_label
             // 
             exit_label.AutoSize = true;
-            exit_label.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            exit_label.Location = new Point(374, 0);
+            exit_label.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            exit_label.Location = new Point(427, 0);
             exit_label.Name = "exit_label";
-            exit_label.Size = new Size(30, 32);
+            exit_label.Size = new Size(38, 41);
             exit_label.TabIndex = 1;
             exit_label.Text = "X";
             exit_label.Click += exit_label_Click;
@@ -199,20 +216,29 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             label4.Location = new Point(0, 0);
             label4.Name = "label4";
-            label4.Size = new Size(148, 32);
+            label4.Size = new Size(182, 41);
             label4.TabIndex = 6;
             label4.Text = "Neues Buch";
             // 
-            // item_name_tb
+            // Item_add_UC
             // 
-            item_name_tb.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            item_name_tb.Location = new Point(175, 47);
-            item_name_tb.Name = "item_name_tb";
-            item_name_tb.Size = new Size(207, 33);
-            item_name_tb.TabIndex = 33;
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Controls.Add(label4);
+            Controls.Add(exit_label);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Item_add_UC";
+            Size = new Size(462, 908);
+            Load += Book_add_UC_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
